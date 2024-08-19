ac4c2ec39ef7572b445cb1e127f40505cc87c864
Robin <hanya8493@gmail.com>
2024-08-19 17:25:02 +0000

    ath10k: Update array null checks to length checks in ath10k_wmi_start_scan_calc_len
    Removed redundant checks for null pointers of arrays in the ath10k_wmi_start_scan_calc_len function. The previous checks for `arg->ie`, `arg->channels`, `arg->ssids`, and `arg->bssids` were unnecessary as array addresses are never null. Replaced these with checks for positive values of corresponding length fields to ensure proper validation and calculation of scan parameters length.
    
    This update resolves compiler warnings related to pointer-bool conversions.
    
    Signed-off-by: Robin <hanya8493@gmail.com>

--- drivers/net/wireless/ath/ath10k/wmi.c
+++ drivers/net/wireless/ath/ath10k/wmi.c
@@ -3265,9 +3265,7 @@ static int ath10k_wmi_start_scan_calc_len(struct ath10k *ar,
 	else
 		len = sizeof(struct wmi_start_scan_cmd);
 
-	if (arg->ie_len) {
-		if (!arg->ie)
-			return -EINVAL;
+	if (arg->ie_len > 0) {
 		if (arg->ie_len > WLAN_SCAN_PARAMS_MAX_IE_LEN)
 			return -EINVAL;
 
@@ -3275,9 +3273,7 @@ static int ath10k_wmi_start_scan_calc_len(struct ath10k *ar,
 		len += roundup(arg->ie_len, 4);
 	}
 
-	if (arg->n_channels) {
-		if (!arg->channels)
-			return -EINVAL;
+	if (arg->n_channels > 0) {
 		if (arg->n_channels > ARRAY_SIZE(arg->channels))
 			return -EINVAL;
 
@@ -3285,9 +3281,7 @@ static int ath10k_wmi_start_scan_calc_len(struct ath10k *ar,
 		len += sizeof(__le32) * arg->n_channels;
 	}
 
-	if (arg->n_ssids) {
-		if (!arg->ssids)
-			return -EINVAL;
+	if (arg->n_ssids > 0) {
 		if (arg->n_ssids > WLAN_SCAN_PARAMS_MAX_SSID)
 			return -EINVAL;
 
@@ -3295,9 +3289,7 @@ static int ath10k_wmi_start_scan_calc_len(struct ath10k *ar,
 		len += sizeof(struct wmi_ssid) * arg->n_ssids;
 	}
 
-	if (arg->n_bssids) {
-		if (!arg->bssids)
-			return -EINVAL;
+	if (arg->n_bssids > 0) {
 		if (arg->n_bssids > WLAN_SCAN_PARAMS_MAX_BSSID)
 			return -EINVAL;
 
